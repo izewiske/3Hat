143f0ddbfb300b48eecb58875fd67b5632e130e4
Thomas-Scott <scotthom15@gmail.com>
2014-01-29 22:43:45 -0600

    trivial: commit to pull

---
+++ src/MultipleContourDriver.cpp
@@ -0,0 +1,17 @@
+/*
+*
+*	This program will load multiple images and process them then determine how well the 
+*
+*/
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "eriolHeaders.h"
+
+int main(){
+	Image currLeftImage;
+	Image currRightImage;
+
+
+}
